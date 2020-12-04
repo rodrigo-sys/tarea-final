e715de4acda531ce262675b534b99ad4e44ef66e
Chavez Rodrigo <chavezrodrigo@tutanota.com>
2020-12-03 21:27:31 -0300

    ahora los botones anterior/siguiente se desactivan cuando deben

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+function activarBoton(selector_boton){
+    document.querySelector(selector_boton).style = '';
+}
+
+function desactivarBoton(selector_boton){
+    document.querySelector(selector_boton).style.background = 'gray';
+    document.querySelector(selector_boton).style.textDecoration = 'none';
+}
+
 textos = document.querySelectorAll("div.texto");
 
 let texto_actual = document.querySelector('#texto_actual');
@@ -14,6 +23,14 @@ boton_siguiente.onclick = function() {
     indice_actual += 1;
   } 
 
+  if(indice_actual != 0){
+    activarBoton("#anterior");
+  }
+
+  if(indice_actual == textos.length - 1){
+    desactivarBoton("#siguiente");
+  } 
+
   texto_actual.innerHTML = textos[indice_actual].innerHTML;
 }
 
@@ -22,5 +39,19 @@ boton_anterior.onclick = function() {
     indice_actual -= 1;
   } 
 
+  if(indice_actual != textos.length - 1){
+    activarBoton("#siguiente");
+  } 
+
+  if(indice_actual == 0){
+    desactivarBoton("#anterior");
+  } 
+
   texto_actual.innerHTML = textos[indice_actual].innerHTML;
 }
+
+if(indice_actual == 0){
+    desactivarBoton("#anterior");
+} 
+
+
